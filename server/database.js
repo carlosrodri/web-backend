3541cbcad1a42990414f64726cc1738a171afc7f
carlosrodri <carlosalberth.cars@gmail.com>
2020-10-03 17:05:11 -0500

    connection with production db

--- server/database.js
+++ server/database.js
@@ -8,21 +8,9 @@
     
     
     const UR = 'mongodb://localhost/web'
-    // const URIM = 'mongodb://admin:tuyxib4rhCdTMZrc@SG-brindis-31778.servers.mongodirector.com:51690,SG-brindis-31779.servers.mongodirector.com:51690,SG-brindis-31780.servers.mongodirector.com:51690/admin?replicaSet=RS-brindis-0&ssl=true'
-    // const MONGO_DB_URI = 'mongodb+srv://admin:abc.52106@cluster0-5cmep.mongodb.net/test?retryWrites=true&w=majority'
-    
-    /*const MongoClient = require('mongod').MongoClient();
-    const uri = "mongodb+srv://admin:abc.52106@cluster0-5cmep.mongodb.net/test?retryWrites=true&w=majority";
-    const client = new MongoClient(uri, { useNewUrlParser: true }, { useUnifiedTopology: true });
-    client.connect(err => {
-      const collection = client.db("test").collection("devices");
-      // perform actions on the collection object
-      client.close();
-    });*/
-    
-    
-    
-    mongoose.connect(UR, {useNewUrlParser: true, useUnifiedTopology: true})
+    const uri = "mongodb+srv://admin:abc.52106@mywebpage.5cmep.mongodb.net/MyWebPage?retryWrites=true&w=majority";
+
+    mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true})
     .then((db) => {
         console.log('DB is connected ' + db);
     }).catch((err) => {
